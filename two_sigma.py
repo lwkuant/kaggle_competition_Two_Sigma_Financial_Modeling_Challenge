d697630b067e77998922404ab65dfd7816f41b2c
lwkuant <lwkuant@gmail.com>
2016-12-28 20:17:03 +0800

    Add two new plots

--- two_sigma.py
+++ two_sigma.py
@@ -27,8 +27,12 @@ with pd.HDFStore("train.h5", "r") as train:
 print(df.shape) # 1710756 rows, 111 variables
 print(df.info())
 print(df.isnull().any()) # check for NAs
-print(np.sum(df.isnull().any()) ) # 106 with NAs
+print(np.sum(df.isnull().any())) # 106 with NAs
 print(df.head())
+print(df.describe())
+
+import os 
+os.chdir(r'D:\Project\kaggle_competition_Two_Sigma_Financial_Modeling_Challenge')
 
 # column type 
 col_name = df.columns
@@ -71,4 +75,60 @@ from scipy.stats import skew
 print(skew(df.y.values)) # skewness is 0.22
 # the distribution of y is nearly normal distributed
 # the relative high frequencies on both of the ends are worth studying
+
+# check the distribution of the timestamp
+print(df.timestamp.isnull().any()) # no NA
+
+import matplotlib
+matplotlib.style.use('ggplot')   
+fig, axes = plt.subplots(figsize=[15, 15])
+axes.tick_params(labelsize=15)
+sns.distplot(df.timestamp, bins=100, kde=False)
+axes.set_title('Frequency Distribution of timestamp', fontsize=30)
+axes.set_xlabel('Value', fontsize=25)
+axes.set_ylabel('Count', fontsize=25)
+# there seemsto have some patterns 
+
+# The number of NAs in each variable 
+num_NAs = df.isnull().sum()
+num_NAs_count = num_NAs.values
+num_NAs_name = num_NAs.index
+
+import matplotlib 
+
+matplotlib.style.use('ggplot') 
+fig, axes = plt.subplots(figsize=[15, 60])
+#axes.bar(list(range(len(num_NAs_name))), num_NAs_count, orientation='vertical')
+axes.tick_params(labelsize=15)
+axes.barh(list(range(len(num_NAs_name))), num_NAs_count, color='#2E8B57')
+axes.set_title('NA frequency for each variable', fontsize=30)
+axes.set_yticks(np.arange(len(num_NAs_name))+0.4)
+axes.set_yticklabels(num_NAs_name)
+axes.set_xlabel('Frequency', fontsize=25)
+axes.set_ylabel('Variable', fontsize=25)
+#plt.savefig('NA_frequency.png', dpi=500)
+# there are some very large numbeers of NAs
+# we have to condier NAs processing carefully
+
+# Correlation between features and the target
+from scipy.stats import pearsonr
+col_name = df.columns
+col_name_feat = col_name[1:-1]
+
+cor = np.array([pearsonr(df.ix[:, [col]].dropna().values,
+                         df.ix[:, ['y']].ix[df.ix[:, [col]].dropna().index, :].values)[0] for col in col_name])
+
+import matplotlib 
+
+matplotlib.style.use('ggplot') 
+fig, axes = plt.subplots(figsize=[15, 60])
+#axes.bar(list(range(len(num_NAs_name))), num_NAs_count, orientation='vertical')
+axes.tick_params(labelsize=15)
+axes.barh(list(range(len(col_name_feat))), cor, color='#2E8B57')
+axes.set_title('Correlation with y for each variable', fontsize=30)
+axes.set_yticks(np.arange(len(col_name_feat))+0.4)
+axes.set_yticklabels(col_name_feat)
+axes.set_xlabel('Correlation', fontsize=25)
+axes.set_ylabel('Variable', fontsize=25)
+#plt.savefig('Correlation_with_y.png', dpi=500)
 
